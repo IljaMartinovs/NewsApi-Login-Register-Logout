8c0efecd38926e7b24d7491fdd9e0d338866fe37
Iļja Martinovs <106473441+IljaMartinovs@users.noreply.github.com>
2022-12-05 10:15:20 +0200

    Update ErrorController.php

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -6,7 +6,7 @@ use App\Template;
 
 class ErrorController
 {
-    public function index(string $errorMessage, string $buttonName, string $buttonLink) //: Template
+    public function index(string $errorMessage, string $buttonName, string $buttonLink) : ?Template
     {
         return (new Template("error.twig", [
                 'errorMessage' => $errorMessage,
